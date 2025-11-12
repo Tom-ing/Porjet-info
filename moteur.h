bbf6afcb50e7a3a55e2dec52c4c05155fb3627e7
remimtd <remi.matteoda@edu.ece.fr>
2025-11-12 14:06:00 +0100

    Create moteur.h

---
+++ moteur.h
@@ -0,0 +1,21 @@
+/*
+* ════════════════════════════════════════════════════════════════════════════
+ * FICHIER: moteur.h
+ * Description: Module moteur de jeu - Header
+ * ════════════════════════════════════════════════════════════════════════════
+ */
+
+#ifndef MOTEUR_H
+#define MOTEUR_H
+
+#include "structures.h"
+
+// Initialisation
+void initialiserPartie(Partie* partie);
+void genererNiveau(Partie* partie, int niveau);
+void remplirTableauAleatoire(int tableau[HAUTEUR][LARGEUR]);
+
+int permuterItems(Partie* partie, int y1, int x1, int y2, int x2);
+int estAdjacent(int y1, int x1, int y2, int x2);
+
+#endif // MOTEUR_H
